2b57249f8c3ecdc7e65fa54fb226414cd60b2303
Wai Tsun Yim <flyerwaitsun@gmail.com>
2015-01-24 17:18:56 +0800

    - Added action_about to menu_mic.xml - Added action_about to menu_main.xml - Added navigation to Settings and About from MicActivity Menu

--- app/src/main/java/sg/edu/nus/micphone2/MicActivity.java
+++ app/src/main/java/sg/edu/nus/micphone2/MicActivity.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.micphone2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,13 +28,32 @@ public class MicActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId())
+        {
+
+            case R.id.action_settings:
+                OpenSetting();
+                return true;
+
+            case R.id.action_about:
+                OpenAbout();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void OpenSetting()
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void OpenAbout()
+    {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
